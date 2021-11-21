312bd5ed19b342a194d18645ca2c7f87a5669970
m-alcu <martin.alcubierre@gmail.com>
2021-11-21 15:59:43 +0100

    tests included

--- serv_layer/api/v1_00/endpoints/posts.py
+++ serv_layer/api/v1_00/endpoints/posts.py
@@ -5,32 +5,32 @@ from models.posts import Post as ModelPost
 
 router = APIRouter()
 
-@router.get("/post/", response_model=List[PostDB])
+@router.get("/post", response_model=List[PostDB])
 async def get_all_post():
     db_posts = await ModelPost.get_all()
     return list(map(lambda db_post: PostDB(**db_post), db_posts))
 
-@router.post("/post/")
+@router.post("/post", status_code=201)
 async def create_post(post: PostSchema):
     post_id = await ModelPost.create(**post.dict())
-    return {"user_id": post_id}
+    return {"id": post_id}
 
 
 @router.get("/post/{id}", response_model=PostDB)
-async def get_post(id: int):
+async def get_post(id: int = Path(..., gt=0),):
     post = await ModelPost.get(id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     return PostDB(**post).dict()
 
-@router.put("/post/{id}/", response_model=None)
+@router.put("/post/{id}", response_model=None)
 async def modify_post(payload: PostSchema, id: int = Path(..., gt=0),):
     post = await ModelPost.get(id)
     if not post:
         raise HTTPException(status_code=404, detail="Post not found")
     await ModelPost.put(id, **payload.dict())
     
-@router.delete("/post/{id}/", response_model=None)
+@router.delete("/post/{id}", response_model=None)
 async def delete_post(id: int = Path(..., gt=0)):
     post = await ModelPost.get(id)
     if not post:
---
+++ serv_layer/tests/test_api/test_posts.py
@@ -0,0 +1,144 @@
+import json
+
+import pytest
+
+from models.posts import Post as ModelPost
+
+
+def test_create_post(test_app, monkeypatch):
+    test_request_payload = {"title": "something", "body": "something else", "is_published": True}
+    test_response_payload = {"id": 1}
+
+    async def mock_post(*args, **kwargs):
+        return 1
+
+    monkeypatch.setattr(ModelPost, "create", mock_post)
+
+    response = test_app.post("/api/post", data=json.dumps(test_request_payload),)
+
+    assert response.status_code == 201
+    assert response.json() == test_response_payload
+
+
+def test_create_note_invalid_json(test_app):
+    response = test_app.post("/api/post", data=json.dumps({"title": "something"}))
+    assert response.status_code == 422
+
+    response = test_app.post("/api/post", data=json.dumps({"title": "1", "body": "2"}))
+    assert response.status_code == 422
+
+
+def test_read_post(test_app, monkeypatch):
+    test_data = {"id": 1, "title": "something", "body": "something else", "is_published": True, "created": None, "modified": None}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    response = test_app.get("/api/post/1")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_read_post_incorrect_id(test_app, monkeypatch):
+
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    response = test_app.get("/api/post/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Post not found"
+
+    response = test_app.get("/api/post/0")
+    assert response.status_code == 422
+
+
+def test_read_all_posts(test_app, monkeypatch):
+    test_data = [
+        {"title": "something", "body": "something else", "id": 1, "is_published" : True, "created": None, "modified": None},
+        {"title": "someone", "body": "someone else", "id": 2, "is_published" : True, "created": None, "modified": None}
+    ]
+
+    async def mock_get_all():
+        return test_data
+
+    monkeypatch.setattr(ModelPost, "get_all", mock_get_all)
+
+    response = test_app.get("/api/post/")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_update_post(test_app, monkeypatch):
+    test_request_payload = {"title": "someone", "body": "someone else", "is_published": True}
+    test_response_payload = {"id": 1}
+
+    async def mock_get(id):
+        return test_response_payload
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    async def mock_put(id, **kwargs):
+        return None
+
+    monkeypatch.setattr(ModelPost, "put", mock_put)
+
+    response = test_app.put("/api/post/1", data=json.dumps(test_request_payload))
+    assert response.status_code == 200
+    assert response.json() == None
+
+
+@pytest.mark.parametrize(
+    "id, payload, status_code",
+    [
+        [1, {}, 422],
+        [1, {"body": "bar"}, 422],
+        [999, {"title": "foo", "body": "bar", "is_published": True}, 404],
+        [1, {"title": "1", "body": "bar"}, 422],
+        [1, {"title": "foo", "body": "1"}, 422],
+        [0, {"title": "foo", "body": "bar"}, 422],
+    ],
+)
+def test_update_post_invalid(test_app, monkeypatch, id, payload, status_code):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    response = test_app.put(f"/api/post/{id}", data=json.dumps(payload),)
+    assert response.status_code == status_code
+
+
+def test_remove_post(test_app, monkeypatch):
+    test_data = {"title": "something", "body": "something else", "id": 1}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    async def mock_delete(id):
+        return None
+
+    monkeypatch.setattr(ModelPost, "delete", mock_delete)
+
+    response = test_app.delete("/api/post/1")
+    assert response.status_code == 200
+    assert response.json() == None
+
+
+def test_remove_post_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(ModelPost, "get", mock_get)
+
+    response = test_app.delete("/api/post/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Post not found"
+
+    response = test_app.delete("/api/post/0")
+    assert response.status_code == 422
